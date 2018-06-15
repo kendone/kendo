f1812b81c3c2ae70a768063c4f879dabb4fdb7ea
kendozoo <nason-dev@outlook.com>
2018-06-15 15:46:00 +0800

    create

---
+++ src/main/java/com/kendo/KendoApplication.java
@@ -0,0 +1,20 @@
+package com.kendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class KendoApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KendoApplication.class, args);
+    }
+}
