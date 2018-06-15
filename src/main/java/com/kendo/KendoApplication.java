43cdc2123b8aadfacd99900b5e088898d3ca4b65
kendozoo <nason-dev@outlook.com>
2018-06-15 16:07:26 +0800

    *

--- src/main/java/com/kendo/KendoApplication.java
+++ src/main/java/com/kendo/KendoApplication.java
@@ -11,7 +11,7 @@ public class KendoApplication {
 
     @RequestMapping("/")
     String home(){
-        return "Hello World!!!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
