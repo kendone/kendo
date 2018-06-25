f642ad6ecf9087bfc29ca41dd63c7923ec7d0ea9
kendone <ken-done@outlook.com>
2018-06-25 16:58:16 +0800

    *

--- src/test/java/com/kendo/KendoApplicationTests.java
+++ src/test/java/com/kendo/KendoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.kendo;
 
+import com.kendo.admin.bean.Menu;
+import com.kendo.admin.mapper.MenuMapper;
 import com.kendo.security.bean.Principal;
 import com.kendo.security.mapper.PrincipalMapper;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Objects;
 
 @RunWith(SpringRunner.class)
@@ -19,7 +22,16 @@ public class KendoApplicationTests {
     private PrincipalMapper principalMapper;
 
     @Autowired
+    private MenuMapper menuMapper;
+
+    @Autowired
     private PasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    public void selectAll(){
+        List<Menu> menus = menuMapper.selectAll();
+        System.out.println(menus.get(0).getCreateUser().getUsername());
+    }
 
     @Test
     public void passwordEncoder() {
