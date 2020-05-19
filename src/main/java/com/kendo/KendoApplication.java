ee01a027122bfcca01be30037d11139397c17e50
kendone <ken-done@outlook.com>
2020-05-19 22:35:53 +0800

    ..

--- src/main/java/com/kendo/KendoApplication.java
+++ src/main/java/com/kendo/KendoApplication.java
@@ -3,6 +3,9 @@ package com.kendo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author kendone
+ */
 @SpringBootApplication
 public class KendoApplication {
 
